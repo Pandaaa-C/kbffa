7c9d9dc776ace1f568235fa527c4ab8a08e80bbb
Pandaaa-C <49002481+Pandaaa-C@users.noreply.github.com>
2025-01-12 23:53:34 +0000

    made double jump buyable in shop

--- src/main/java/com/panda0day/kbffa/addons/AddonManager.java
+++ src/main/java/com/panda0day/kbffa/addons/AddonManager.java
@@ -1,11 +1,18 @@
 package com.panda0day.kbffa.addons;
 
+import com.panda0day.kbffa.Main;
+import com.panda0day.kbffa.managers.CoinsManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.sql.Connection;
 import java.util.*;
 
 public class AddonManager {
@@ -17,6 +24,24 @@ public class AddonManager {
 
     public AddonManager() {
         this.addons.add(new DoubleJumpAddon());
+
+        Main.getInstance().getServer().getScheduler().runTaskTimer(Main.getInstance(), new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    if (hasActiveAddon(player)) {
+                        if (isAddonExpired(player)) {
+                            removePlayer(player);
+
+                            Addon addon = getAddon(player);
+                            if (addon == null) return;
+
+                            player.sendMessage(Main.getMainConfig().getPrefix() + ChatColor.RED + "Your Addon " + ChatColor.GOLD + addon.getAddonName() + " has expired!");
+                        }
+                    }
+                });
+            }
+        }, 0L, 20L * 5);
     }
 
     public void addPlayer(Player player, Addon addon) {
@@ -25,6 +50,10 @@ public class AddonManager {
 
     public void removePlayer(Player player) {
         this.playerAddons.remove(player);
+    }
+
+    public boolean addonActive(Player player, String addonName) {
+        return this.playerAddons.containsKey(player) && this.playerAddons.get(player).getAddonName().equals(addonName);
     }
 
     public boolean hasActiveAddon(Player player) {
@@ -59,5 +88,35 @@ public class AddonManager {
         if (!inventoryView.getTitle().equalsIgnoreCase("Addon Shop")) return;
 
         event.setCancelled(true);
+
+        ItemStack clickedItem = event.getCurrentItem();
+        if (clickedItem == null) return;
+
+        ItemMeta clickedItemMeta = clickedItem.getItemMeta();
+        if (clickedItemMeta == null) return;
+
+        switch (clickedItemMeta.getDisplayName()) {
+            case "§6§lDouble Jump":
+                if (hasActiveAddon(player)) {
+                    player.sendMessage(Main.getMainConfig().getPrefix() + ChatColor.RED + "You already have an Addon active!");
+                    return;
+                }
+
+                if (isAddonExpired(player)) {
+                    removePlayer(player);
+                }
+
+                DoubleJumpAddon addon = new DoubleJumpAddon();
+                int coins = CoinsManager.getCoins(player.getUniqueId());
+                if (coins >= addon.getAddonPrice()) {
+                    CoinsManager.removeCoins(player.getUniqueId(), addon.getAddonPrice());
+                    player.sendMessage(Main.getMainConfig().getPrefix() + ChatColor.GREEN + "You have bought the Addon " + ChatColor.GOLD + addon.getAddonName());
+                    addPlayer(player, addon);
+                    addon.allowFlight(player);
+                    player.playSound(player.getLocation(), Sound.BLOCK_ENCHANTMENT_TABLE_USE, 1f, 1f);
+                }
+                break;
+            default: break;
+        }
     }
 }
