0b9e0b79ffded5b1acb1fe0e0c5743cc1e5a2763
Pandaaa-C <49002481+Pandaaa-C@users.noreply.github.com>
2025-01-12 23:15:31 +0000

    add addon object to doublejump

--- src/main/java/com/panda0day/kbffa/addons/DoubleJumpAddon.java
+++ src/main/java/com/panda0day/kbffa/addons/DoubleJumpAddon.java
@@ -1,15 +1,32 @@
 package com.panda0day.kbffa.addons;
 
+import com.panda0day.kbffa.managers.ItemManager;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class DoubleJumpAddon {
+public class DoubleJumpAddon extends Addon {
     public Map<Player, Date> activeList;
 
     public DoubleJumpAddon() {
+        super(
+                "Double Jump",
+                new ItemManager(Material.DIAMOND_BOOTS)
+                        .addEnchantment(Enchantment.UNBREAKING, 10)
+                        .setDisplayName("§6§lDouble Jump")
+                        .setLore(List.of(
+                                "§aGet a Double Jump Effect",
+                                "§afor §6§l5 minutes",
+                                "§bFor 5000 Coins"
+                        ))
+                        .create(),
+                5000
+        );
         this.activeList = new HashMap<>();
     }
 
